f4fab4f9c40501e8995d5c0af0134bc5efc0cd93
Erez Shinan <erez27@gmail.com>
2009-12-08 23:15:10 +0000

    fixed issue 18 (Allow to import python files sitting in the same directory as pybw.dll)

--- Dllmain.cpp
+++ Dllmain.cpp
@@ -10,13 +10,24 @@
 
 void python_wrap_init();
 
+void add_module_path_to_pythonpath(HMODULE module)
+{
+	char filename[MAX_PATH];
+	char python_command[MAX_PATH + 100];
+	GetModuleFileNameA( module, filename, MAX_PATH);
+	_snprintf_s(python_command, MAX_PATH+100, MAX_PATH+100, "module_filename = r'''%s'''", filename);
+	PyRun_SimpleString(python_command);
+	PyRun_SimpleString("import sys; sys.path.insert(0, module_filename.rsplit('\\\\', 1)[0])");
+}
+
 #include "AIModuleImpl.h"
 namespace BWAPI { Game* Broodwar = NULL; }
-BOOL APIENTRY DllMain( HANDLE module, 
+BOOL APIENTRY DllMain( HMODULE module, 
                        DWORD  reason_for_call, 
                        LPVOID reserved
 					 )
 {
+
 	UNREFERENCED_PARAMETER( reserved );
 	UNREFERENCED_PARAMETER( module );
     
@@ -26,6 +37,7 @@ BOOL APIENTRY DllMain( HANDLE module,
         BWAPI::BWAPI_init();
 		Py_Initialize();
 		python_wrap_init();
+		add_module_path_to_pythonpath( module );
 		break;
 	case DLL_PROCESS_DETACH:
 		Py_Finalize();
